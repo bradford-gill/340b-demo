4e902510c46711091a1471d52e9b0f2dc6dbee00
Bradford Gill <brad@blackmore.ai>
2026-04-24 10:56:01 -0400

    Add it up

--- deploy.sh
+++ deploy.sh
@@ -7,19 +7,17 @@ SERVER_HOST="54.210.244.233"
 SSH_KEY="$HOME/Desktop/LightsailDefaultKey-us-east-1.pem"
 REMOTE_DIR="/home/ubuntu/340b-demo"
 
-echo "Building..."
-cd project && npm run build && cd ..
-
-echo "Syncing to $SERVER_HOST..."
+echo "Syncing source to $SERVER_HOST..."
 rsync -az --delete -e "ssh -i $SSH_KEY" \
-  project/dist/ "$SERVER_USER@$SERVER_HOST:$REMOTE_DIR/dist/"
+  --exclude='node_modules' --exclude='dist' \
+  project/ "$SERVER_USER@$SERVER_HOST:$REMOTE_DIR/project/"
 
 rsync -az -e "ssh -i $SSH_KEY" \
-  docker-compose.yml project/Caddyfile \
+  docker-compose.yml Dockerfile \
   "$SERVER_USER@$SERVER_HOST:$REMOTE_DIR/"
 
-echo "Restarting container..."
+echo "Building and restarting on server..."
 ssh -i "$SSH_KEY" "$SERVER_USER@$SERVER_HOST" \
-  "cd $REMOTE_DIR && docker compose up -d"
+  "cd $REMOTE_DIR && docker compose up -d --build"
 
 echo "Done! Site is live."
